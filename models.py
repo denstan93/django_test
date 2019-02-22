3f7ad2b6a6297db071affa694bbf557d9155423b
denstan93 <45799832+denstan93@users.noreply.github.com>
2019-02-22 19:32:35 +0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class BarberShop(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    addres = models.CharField(max_length=100)
+    time_work = models.CharField(max_length=100)
+    raiting = models.IntegerField(default=0)
+
+
+class Master(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    work_place = models.CharField(max_length=100)
+    raiting = models.IntegerField(default=0)
+    experience = models.IntegerField(default=0)
+    portfolio = models.CharField(max_length=100)
+
+
+class recording(models.Model):
+    data = models.DateTimeField(max_length=100)
+    time = models.TimeField(max_length=100)
+    name_clieint = models.ForeignKey(Client, on_delete=models.CASCADE)
+    name_master = models.ForeignKey(Master, on_delete=models.CASCADE)
+
+
+class Client(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    raiting = models.IntegerField(default=0)
+    history = models.CharField(max_length=100)
